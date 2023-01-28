e4ba29fefbc393ea5ab451fc1a81586bc2274360
ciciAlexander97 <caden@hcbbhome.com>
2023-01-27 18:34:48 -0600

    Made Balls Bounce in random directions

--- src/BallPanel.java
+++ src/BallPanel.java
@@ -13,7 +13,7 @@ public class BallPanel extends JPanel{
     //private int ySpeed2 = 3;
 
     //static Ball b1 = new Ball(30, 150, 20);
-    static Ball[] ballz = new Ball[1000];
+    static Ball[] ballz = new Ball[20];
     private final Ball silly;
 
     public BallPanel(){
@@ -57,24 +57,46 @@ public class BallPanel extends JPanel{
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.setColor(silly.getColor());
-        g.fillOval(silly.getX(), silly.getY(), silly.getSize(), silly.getSize());
+        //g.setColor(silly.getColor());
+        //g.fillOval(silly.getX(), silly.getY(), silly.getSize(), silly.getSize());
         for(int i = 0;i<ballz.length;i++){
             g.setColor(ballz[i].getColor());
             g.fillOval(ballz[i].getX(), ballz[i].getY(), ballz[i].getSize(), ballz[i].getSize());
             ballz[i].moveX(ballz[i].getxSpeed());
             ballz[i].moveY(ballz[i].getySpeed());
-            //if(ballz[i].getX() >= getWidth()-ballz[i].getSize() || ballz[i].getX()<=0){
-            //    ballz[i].setxSpeed(ballz[i].getxSpeed()*-1);
+            if(ballz[i].getX() >= getWidth()-ballz[i].getSize() || ballz[i].getX()<=0){
+                if(ballz[i].getX() >= getWidth()-ballz[i].getSize()){
+                    ballz[i].setX(getWidth()-ballz[i].getSize());
+                }if(ballz[i].getX()<=0){
+                    ballz[i].setX(0);
+                }
+                ballz[i].setxSpeed(ballz[i].getxSpeed()*-1);
+                int rand = (int)(Math.random()*2);
+                if(rand == 0) {
+                    ballz[i].setySpeed((int) (Math.random() * 8));
+                }else{
+                    ballz[i].setySpeed(-(int) (Math.random() * 8));
+                }
+            }
+            //if(ballz[i].getX()>=getWidth()){
+            //    ballz[i].setX(-ballz[i].getSize());
             //}
-            if(ballz[i].getX()>=getWidth()){
-                ballz[i].setX(-ballz[i].getSize());
-            }
-            if(ballz[i].getX()<-ballz[i].getSize()){
-                ballz[i].setX(getWidth());
-            }
+            //if(ballz[i].getX()<-ballz[i].getSize()){
+            //    ballz[i].setX(getWidth());
+            //}
             if(ballz[i].getY() >= getHeight()-ballz[i].getSize() || ballz[i].getY()<=0){
+                if(ballz[i].getY() >= getHeight()-ballz[i].getSize()){
+                    ballz[i].setY(getHeight()-ballz[i].getSize());
+                }if(ballz[i].getY()<=0){
+                    ballz[i].setY(0);
+                }
                 ballz[i].setySpeed(ballz[i].getySpeed()*-1);
+                int rand = (int)(Math.random()*2);
+                if(rand == 0) {
+                    ballz[i].setxSpeed((int) (Math.random() * 8));
+                }else{
+                    ballz[i].setxSpeed(-(int) (Math.random() * 8));
+                }
             }
         }
 
